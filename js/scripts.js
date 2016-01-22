585bf4839594fbc22ac372506f16f0aa15aa4d66
Daren Schaad <darenschaad@gmail.com>
2016-01-22 09:18:06 -0800

    refactor code, got rid of two nested for loops that were redundant

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,14 @@ var pingPong = function(countTo){
   for (var i = 1 ; i <= parseInt(countTo); i ++){
     array.push(i);
     for (var j = 0 ; j < array.length ; j ++){
-      if (array[j] % 3 === 0){
+      if (array[j] % 15 === 0){
+        array.splice(j,1,"pingpong");
+      }
+      else if (array[j] % 3 === 0){
         array.splice(j,1,"ping");
       }
-      for (var k = 0 ; k <array.length ; k ++){
-        if (array[k] % 5 === 0){
-          array.splice(k,1,"pong");
-        }
-        for (var l = 0 ; l < array.length ; l ++){
-          if (array[l] % 15 === 0){
-            array.splice(l,1,"pingpong");
-          }
-        }
+      else if (array[j] % 5 === 0){
+        array.splice(j,1,"pong");
       }
     }
   }
