5e49bec8fac7a23501f6e90d4093b25f041bf210
Daren Schaad <darenschaad@gmail.com>
2016-01-22 08:56:26 -0800

    get the thrid spec to pass, replace numbers divisible by 5 with 'pong'

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,12 @@ var pingPong = function(countTo){
     array.push(i);
     for (var j =0; j < array.length; j ++){
       if (array[j] % 3 === 0){
-        array.splice(j,1,"ping")
+        array.splice(j,1,"ping");
+      }
+      for (var k=0; k <array.length; k ++){
+        if (array[k] % 5 === 0){
+          array.splice(k,1,"pong");
+        }
       }
     }
   }
--- spec/specs.js
+++ spec/specs.js
@@ -5,4 +5,7 @@ describe("pingPong", function(){
   it("will insert ping for numbers divisible by 3", function(){
     expect(pingPong("4")).to.eql([1,2,"ping",4]);
   });
+  it("will insert pong for numbers divisible by 5", function(){
+    expect(pingPong("6")).to.eql([1,2,"ping",4,"pong","ping"]);
+  });
 });
