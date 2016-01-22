8efcf9d7e10d2668c76624ed46cc56bbfad3b151
Daren Schaad <darenschaad@gmail.com>
2016-01-22 08:52:09 -0800

    get the second spec to pass, replace numbers divisible by 3 with 'ping'

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,11 @@ var pingPong = function(countTo){
   var array = [];
   for (var i = 1; i <= parseInt(countTo); i ++){
     array.push(i);
+    for (var j =0; j < array.length; j ++){
+      if (array[j] % 3 === 0){
+        array.splice(j,1,"ping")
+      }
+    }
   }
   return array;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,8 @@
 describe("pingPong", function(){
   it("will count up to a certain number", function(){
-    expect(pingPong("4")).to.eql([1,2,3,4]);
+    expect(pingPong("2")).to.eql([1,2]);
+  });
+  it("will insert ping for numbers divisible by 3", function(){
+    expect(pingPong("4")).to.eql([1,2,"ping",4]);
   });
 });
