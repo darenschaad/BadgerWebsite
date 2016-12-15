152e218f3993e05eefd89c9445aad7bdeab17dc5
Daren Schaad <darenschaad@gmail.com>
2016-12-14 17:56:52 -0800

    create badge function is working, just need to add a promise to query the database for the last id used

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,24 @@
-function createBadge(name, index, latitude, longitude, rating, imageUrl, tags, challenges) {
+function createBadge(name, index, latitude, longitude, imageUrl, description, proof, comments, category, creator, pushId, date, tags, challenges, originalIndex) {
   // this needs to be .set() with the last badge query + 1 for the id
-  var newRef = firebase.database().ref('badges/').push();
-  var pushId = getLastId() + 1;
+  // var newRef = firebase.database().ref('badges/').push();
+
   var firebaseObject = {
     name: name,
     index: index,
     latitude: latitude,
     longitude: longitude,
-    rating: rating,
     pushId: pushId,
     imageUrl: imageUrl,
+    description: description,
+    proof: proof,
+    comments: comments,
+    category: category,
+    creator: creator,
+    date: date,
     tags: tags,
-    challenges: challenges
+    challenges: challenges,
+    originalIndex: originalIndex
   }
-  // for (var i = 0; i < tagsArray.length; i++) {
-  //   firebaseObject.tags[i] = tagsArray[i];
-  // }
   firebase.database().ref('badges/' + pushId).set(firebaseObject);
 }
 
@@ -56,143 +59,90 @@ function searchBadge(dewey) {
       $("#badgeEditLongitude").val(badge.longitude);
       $("#badgeEditDewey").val(visibleIndex);
       $("#badgeEditCategory").val(badge.category);
-      $("#edit-id").val(badge.pushId)
+      $("#badge-edit-id").val(badge.pushId)
       $("#badgeEditCreator").val(badge.creator);
       $("#badgeEditDate").val(badge.date);
       $("#badgeEditTags").val(badge.tags);
       $("#badgeEditChallenges").val(badge.challenges);
     })
   })
-
-}
-
-function receivedText(e) {
-  var lines = e.target.result;
-  var newObject = JSON.parse(lines);
-  console.log(newArray);
-  var newArray = newObject.tags;
-  for(var i = 0; i < newArray.length; i++) {
-    var tagsObject = newArray[i];
-    var ref = firebase.database().ref('badges/' + i + "/tags/");
-      var badgeTagsObject = {};
-      for(var key in tagsObject) {
-        if(tagsObject.hasOwnProperty(key)) {
-          console.log(tagsObject[key]);
-          if(key !== "name" && tagsObject[key] !== "") {
-            badgeTagsObject[key] = tagsObject[key];
-          }
-        }
-      }
-      console.log(badgeTagsObject);
-      ref.set(badgeTagsObject);
-  }
-}
-
-function base64(file, callback){
-  var coolFile = {};
-  function readerOnload(e){
-    var base64 = btoa(e.target.result);
-    coolFile.base64 = base64;
-    callback(coolFile)
-  };
-
-  var reader = new FileReader();
-  reader.onload = readerOnload;
-  // var file = file[0].files[0];
-  coolFile.filetype = file.type;
-  coolFile.size = file.size;
-  coolFile.filename = file.name;
-  reader.readAsBinaryString(file);
 }
 
 function getLastId() {
   var query = firebase.database().ref('badges').orderByKey().limitToLast(1).on("child_added", function(snapshot) {
+    // console.log(snapshot.key);
     return (snapshot.key);
   });
 }
 
-function uploadImages(imageArray) {
-
-  for (var i = 0; i < imageArray.length; i++) {
-    firebase.database().ref('images/' + i + '/imageUrl').set(imageArray[i]);
-  }
-}
+// function uploadImages(imageArray) {
+//
+//   for (var i = 0; i < imageArray.length; i++) {
+//     firebase.database().ref('images/' + i + '/imageUrl').set(imageArray[i]);
+//   }
+// }
 
 $(document).ready(function(){
+  // var nextId = Number(getLastId() + 1);
 
-  function setEditFormFields(badge){
-    $("#badgeSearchName").val(badge.name);
-  }
+  // function setEditFormFields(badge){
+  //   $("#badgeSearchName").val(badge.name);
+  // }
   $("form.search-form").submit(function(event){
     event.preventDefault();
     var searchDewey = "a" + $("#badgeSearchDewey").val();
     searchBadge(searchDewey);
-
-
   })
   $("#searchEditButton").click(function() {
     $("#editForm").show();
+
   })
-  var csv = [];
 
-  $("form#badgeForm").submit(function(event){
+  $("form#badge-form").submit(function(event){
     event.preventDefault();
-    var image64;
-    var imageFiles = $("#imageFile");
-    console.log(imageFiles);
-    var imageFilesLength = imageFiles[0].files.length;
 
+      var name = $("#badge-name").val();
+      var index = "a" + $("#badge-index").val();
+      var latitude = Number($("#badge-latitude").val());
+      var longitude = Number($("#badge-longitude").val());
+      var imageUrl = $("#badge-imageUrl").val();
+      var description = $("#badge-description").val();
+      var originalIndex = Number($("#badge-index").val())
+      var proof = $("#badge-proof").val();
+      var comments = $("#badge-comments").val();
+      var category = parseInt($("#badge-category").val());
+      var creator = $("#badge-creator").val();
+      var date = $("#badge-date").val();
+      var tags = $("#badge-tags").val();
+      var challenges = $("#badge-challenges").val();
+      // console.log("test" + getLastId());
+      var pushId = 330;
 
-    for (var i = 0; i < imageFilesLength; i++) {
-      base64(imageFiles[0].files[i], function(data){
-        csv.push(data.base64);
-        console.log(i);
+      // pushId.then(
 
-      })
-    }
-
-    // base64( $("#imageFile"), function(data){
-    //   image64 = data.base64;
-    //   var name = $("#badgeName").val();
-    //   var index = $("#badgeIndex").val();
-    //   var latitude = $("#badgeLatitude").val();
-    //   var longitude = $("#badgeLongitude").val();
-    //   var rating = $("#badgeRating").val();
-    //
-    //   createBadge(name, index, latitude, longitude, rating, image64, tagsArray);
-    // })
-
-
-    // var badgeId = "-KHLTOv7JMjOzRePqJLQ";
-    // return firebase.database().ref('/badges/' + badgeId).once('value').then(function(snapshot) {
-    //   var badgename = snapshot.val().name;
-    //   console.log(badgename);
-      // ...
-
-    // });
-
+        createBadge(name, index, latitude, longitude, imageUrl, description, proof, comments, category, creator, pushId, date, tags, challenges, originalIndex);
   });
-  var tagsArray = [];
-  $("#formJson").submit(function(event){
-    event.preventDefault();
-    console.log(csv);
-    uploadImages(csv);
-    var tagsJson = $("#fileJson");
-    var tagsFile = tagsJson[0].files[0];
-    var fr = new FileReader();
-
-    fr.onload = receivedText;
-    fr.readAsText(tagsFile);
-  })
+  // var tagsArray = [];
+  // $("#formJson").submit(function(event){
+  //   event.preventDefault();
+  //   console.log(csv);
+  //   uploadImages(csv);
+  //   var tagsJson = $("#fileJson");
+  //   var tagsFile = tagsJson[0].files[0];
+  //   var fr = new FileReader();
+  //
+  //   fr.onload = receivedText;
+  //   fr.readAsText(tagsFile);
+  // })
 
   $('form#editForm').submit(function(event){
     event.preventDefault();
     var imageUrl = $("#edit-image-url").val();
     var name = $("#badgeEditName").val();
     var index = "a" + $("#badgeEditDewey").val();
-    var latitude = Number($("#badgeLatitude").val());
-    var longitude = Number($("#badgeLongitude").val());
-    var pushId = parseInt($("#edit-id").val());
+    var latitude = Number($("#badgeEditLatitude").val());
+    var longitude = Number($("#badgeEditLongitude").val());
+    var pushId = Number($("#badge-edit-id").val());
     var proof = $("#badgeEditProof").val();
     var description = $("#badgeEditDescription").val();
     var comments = $("#badgeEditComments").val();
@@ -206,14 +156,14 @@ $(document).ready(function(){
 
 
 
-  $("form#tagForm").submit(function(event){
-    event.preventDefault();
-    var newTag = $('#badgeTag').val();
-    $("#tags").append('<li>' + newTag + '</li>');
-    tagsArray.push(newTag);
-    $('#badgeTag').val("");
-    console.log(tagsArray);
-  });
+  // $("form#tagForm").submit(function(event){
+  //   event.preventDefault();
+  //   var newTag = $('#badgeTag').val();
+  //   $("#tags").append('<li>' + newTag + '</li>');
+  //   tagsArray.push(newTag);
+  //   $('#badgeTag').val("");
+  //   console.log(tagsArray);
+  // });
 
 
-});
+})
